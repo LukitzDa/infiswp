7c0435a22f54570640ee02e232b96c66341abe8e
Luka Nadibaidze <NAD230107@spengergasse.at>
2025-03-03 11:01:28 +0100

    change

---
+++ 2025-03-03/hü.sql
@@ -0,0 +1,13 @@
+Leetcode 2 Aufgaben
+
+Aufgabe 6 Replace Employee ID With The Unique Identifier:
+SELECT unique_id, name
+FROM employees e
+LEFT JOIN EmployeeUni eu
+ON e.id = eu.id
+
+Aufgabe 7: Product Sales Analysis I:
+SELECT p.product_name, s.year, s.price
+FROM sales s
+LEFT JOIN product p
+USING(product_id)
